719a41fdda2534f0a3d85d8da4012cb9131bfb83
Henry Nguyen <nguyenhenry682@gmail.com>
2023-10-27 18:51:29 -0500

    Created express server and connection to mongodb

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,9 +1,8 @@
 import express from "express";
 import { config } from 'dotenv';
-
 config();
-
 //Initializing express server
 const app = express();
 app.use(express.json());
 app.listen(5000, () => console.log("Server listening on port 5000"));
+//# sourceMappingURL=index.js.map
