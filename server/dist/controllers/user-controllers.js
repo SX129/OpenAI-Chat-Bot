71efc2564744382953fc57f57258743d2cc838dd
Henry Nguyen <nguyenhenry682@gmail.com>
2023-10-30 17:16:12 -0500

    Added signup function using schema

--- server/dist/controllers/user-controllers.js
+++ server/dist/controllers/user-controllers.js
@@ -1,4 +1,6 @@
 import User from "../models/User.js";
+import { hash } from 'bcrypt';
+//GET request function for all users
 export const getAllUsers = async (req, res, next) => {
     try {
         const users = await User.find();
@@ -9,10 +11,16 @@ export const getAllUsers = async (req, res, next) => {
         return res.status(200).json({ message: "ERROR", cause: error.message });
     }
 };
+//POST request function for signup
 export const userSignup = async (req, res, next) => {
     try {
-        const users = await User.find();
-        return res.status(200).json({ message: "OK", users });
+        const { name, email, password } = req.body;
+        //Encrptying password
+        const hashedPassword = await hash(password, 10);
+        //Creating a new user and saving it
+        const user = new User({ name, email, hashedPassword });
+        await user.save();
+        return res.status(200).json({ message: "OK", id: user._id.toString() });
     }
     catch (error) {
         console.log(error);
--- server/src/controllers/user-controllers.ts
+++ server/src/controllers/user-controllers.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Response, Request } from "express";
 import User from "../models/User.js";
+import { hash } from 'bcrypt';
 
+//GET request function for all users
 export const getAllUsers = async (req:Request, res:Response, next:NextFunction) => {
     try {
         const users = await User.find();
@@ -12,11 +14,19 @@ export const getAllUsers = async (req:Request, res:Response, next:NextFunction) 
     }
 };
 
+//POST request function for signup
 export const userSignup = async (req:Request, res:Response, next:NextFunction) => {
     try {
-        const users = await User.find();
+        const { name, email, password } = req.body;
 
-        return res.status(200).json({ message: "OK", users });
+        //Encrptying password
+        const hashedPassword = await hash(password, 10);
+
+        //Creating a new user and saving it
+        const user = new User({name, email, hashedPassword});
+        await user.save();
+
+        return res.status(200).json({ message: "OK", id:user._id.toString() });
     } catch (error) {
         console.log(error);
         return res.status(200).json({ message: "ERROR", cause:error.message });
