832b651df2d52047d41e56220d760b04d5594b1b
Henry Nguyen <nguyenhenry682@gmail.com>
2023-10-30 22:16:49 -0500

    Created logo component and added to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Logo from './shared/Logo';
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <AppBar sx={{ bccolor: "transparent", position:"static", boxShadow: "none" }}>
+      <Toolbar sx={{display: "flex", }}>
+        <Logo />
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header
