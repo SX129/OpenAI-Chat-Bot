3d87692960e2b11093dd5472513a5848002bae4d
Henry Nguyen <nguyenhenry682@gmail.com>
2023-10-30 22:25:25 -0500

    Adjusted header style

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,7 +5,7 @@ import Logo from './shared/Logo';
 
 function Header() {
   return (
-    <AppBar sx={{ bccolor: "transparent", position:"static", boxShadow: "none" }}>
+    <AppBar sx={{ bgcolor: "transparent", position:"static", boxShadow: "none" }}>
       <Toolbar sx={{display: "flex", }}>
         <Logo />
       </Toolbar>
