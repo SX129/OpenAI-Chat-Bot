35a2e30018c4bcf4be666d14d5f3e149c3e56522
Henry Nguyen <nguyenhenry682@gmail.com>
2023-10-30 19:35:15 -0500

    Added validator middleware check for signup req

--- server/dist/routes/user-routes.js
+++ server/dist/routes/user-routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getAllUsers, userSignup } from "../controllers/user-controllers.js";
+import { signupValidator, validate } from "../utils/validators.js";
 //Router for user endpoints
 const userRoutes = Router();
 //User endpoint requests
 userRoutes.get("/", getAllUsers);
-userRoutes.post("/signup", userSignup);
+userRoutes.post("/signup", validate(signupValidator), userSignup);
 export default userRoutes;
 //# sourceMappingURL=user-routes.js.map
--- server/src/routes/user-routes.ts
+++ server/src/routes/user-routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getAllUsers, userSignup } from "../controllers/user-controllers.js";
+import { signupValidator, validate } from "../utils/validators.js";
 
 //Router for user endpoints
 const userRoutes = Router();
 
 //User endpoint requests
 userRoutes.get("/", getAllUsers);
-userRoutes.post("/signup", userSignup);
+userRoutes.post("/signup", validate(signupValidator), userSignup);
 
 export default userRoutes;
