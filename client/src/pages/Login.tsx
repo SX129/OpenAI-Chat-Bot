933cf1536a17aeb4be65cd18a214f7591ce297ab
Henry Nguyen <nguyenhenry682@gmail.com>
2023-10-31 13:48:09 -0500

    Added login input field for email and password

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import CustomizedInput from '../components/shared/CustomizedInput';
 
 const Login = () => {
   return (
@@ -7,7 +8,17 @@ const Login = () => {
       <Box padding={8} mt={8} display={{md:"flex", sm:"none", xs:"none"}}>
         <img src='robot.png' alt='Robot' style={{width: "400px"}}/>
       </Box>
-      <Box></Box>
+      <Box display={'flex'} flex={{ xs: 1, md: 0.5 }} justifyContent={'center'} alignItems={'center'} padding={2} ml={'auto'} mt={16}>
+        <form style={{margin:'auto', padding:'30px', boxShadow:"10px 10px 20px #000", borderRadius:"10px", border: "none"}}>
+          <Box sx={{display:'flex', flexDirection:"column", justifyContent:'center'}}>
+            <Typography variant='h4' textAlign="center" padding={2} fontWeight={600}>
+              Login
+            </Typography>
+            <CustomizedInput type='email' name='email' label='Email' />
+            <CustomizedInput type='password' name='password' label='Password' />
+          </Box>
+        </form>
+      </Box>
     </Box>
   )
 }
