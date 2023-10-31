1704c001b8a899d3003a4526ce746bdea07cb30b
Henry Nguyen <nguyenhenry682@gmail.com>
2023-10-31 14:24:58 -0500

    Added toast notification to login state

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,14 +2,26 @@ import React from "react";
 import { IoIosLogIn } from "react-icons/io";
 import { Box, Typography, Button } from "@mui/material";
 import CustomizedInput from "../components/shared/CustomizedInput";
+import { toast } from "react-hot-toast";
+import { useAuth } from "../context/AuthContext";
 
 const Login = () => {
+  const auth = useAuth();
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget);
-    const email = formData.get("email");
-    const password = formData.get("password");
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    try {
+      toast.loading("Signing In", { id: "login" });
+      await auth?.login(email, password);
+
+      toast.success("Signed In Successfully", { id: "login" });
+    } catch (error) {
+      console.log(error);
+      toast.error("Signed In Failed", { id: "login" });
+    }
   };
 
   return (
