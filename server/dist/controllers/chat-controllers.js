39a2688ce49c046e6b45c4a61841be8a1fca7857
Henry Nguyen <nguyenhenry682@gmail.com>
2023-10-31 18:27:06 -0500

    Creating chat response array for gpt-3.5

--- server/dist/controllers/chat-controllers.js
+++ server/dist/controllers/chat-controllers.js
@@ -1,6 +1,23 @@
 import User from "../models/User.js";
+import { configureOpenAI } from "../config/openai-config.js";
+import { OpenAIApi } from "openai";
 export const generateChatCompletion = async (req, res, next) => {
     const { message } = req.body;
     const user = await User.findById(res.locals.jwtData.id);
+    if (!user) {
+        return res.status(401).json({ message: "User not registered." });
+    }
+    const chats = user.chats.map(({ role, content }) => ({
+        role,
+        content,
+    }));
+    chats.push({ content: message, role: "user" });
+    user.chats.push({ content: message, role: "user" });
+    const config = configureOpenAI();
+    const openai = new OpenAIApi(config);
+    const chatResponse = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: chats,
+    });
 };
 //# sourceMappingURL=chat-controllers.js.map
--- server/src/controllers/chat-controllers.ts
+++ server/src/controllers/chat-controllers.ts
@@ -1,12 +1,32 @@
 import { NextFunction, Response, Request } from "express";
 import User from "../models/User.js";
+import { configureOpenAI } from "../config/openai-config.js";
+import { OpenAIApi, ChatCompletionRequestMessage } from "openai";
 
 export const generateChatCompletion = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-    const { message } = req.body;
-    const user = await User.findById(res.locals.jwtData.id);
-    
+  const { message } = req.body;
+  const user = await User.findById(res.locals.jwtData.id);
+
+  if (!user) {
+    return res.status(401).json({ message: "User not registered." });
+  }
+
+  const chats = user.chats.map(({ role, content }) => ({
+    role,
+    content,
+  })) as ChatCompletionRequestMessage[];
+
+  chats.push({ content: message, role: "user" });
+  user.chats.push({ content: message, role: "user" });
+
+  const config = configureOpenAI();
+  const openai = new OpenAIApi(config);
+  const chatResponse = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: chats,
+  });
 };
