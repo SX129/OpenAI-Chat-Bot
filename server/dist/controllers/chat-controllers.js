c080d7a727e17acd13e37048bdbb7c567bdb8895
Henry Nguyen <nguyenhenry682@gmail.com>
2023-10-31 18:30:54 -0500

    Refactored code in a try...catch

--- server/dist/controllers/chat-controllers.js
+++ server/dist/controllers/chat-controllers.js
@@ -3,21 +3,30 @@ import { configureOpenAI } from "../config/openai-config.js";
 import { OpenAIApi } from "openai";
 export const generateChatCompletion = async (req, res, next) => {
     const { message } = req.body;
-    const user = await User.findById(res.locals.jwtData.id);
-    if (!user) {
-        return res.status(401).json({ message: "User not registered." });
+    try {
+        const user = await User.findById(res.locals.jwtData.id);
+        if (!user) {
+            return res.status(401).json({ message: "User not registered." });
+        }
+        const chats = user.chats.map(({ role, content }) => ({
+            role,
+            content,
+        }));
+        chats.push({ content: message, role: "user" });
+        user.chats.push({ content: message, role: "user" });
+        const config = configureOpenAI();
+        const openai = new OpenAIApi(config);
+        const chatResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: chats,
+        });
+        user.chats.push(chatResponse.data.choices[0].message);
+        await user.save();
+        return res.status(200).json({ chats: user.chats });
     }
-    const chats = user.chats.map(({ role, content }) => ({
-        role,
-        content,
-    }));
-    chats.push({ content: message, role: "user" });
-    user.chats.push({ content: message, role: "user" });
-    const config = configureOpenAI();
-    const openai = new OpenAIApi(config);
-    const chatResponse = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: chats,
-    });
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Something went wrong." });
+    }
 };
 //# sourceMappingURL=chat-controllers.js.map
--- server/src/controllers/chat-controllers.ts
+++ server/src/controllers/chat-controllers.ts
@@ -9,24 +9,32 @@ export const generateChatCompletion = async (
   next: NextFunction
 ) => {
   const { message } = req.body;
-  const user = await User.findById(res.locals.jwtData.id);
+  try {
+    const user = await User.findById(res.locals.jwtData.id);
 
-  if (!user) {
-    return res.status(401).json({ message: "User not registered." });
+    if (!user) {
+      return res.status(401).json({ message: "User not registered." });
+    }
+
+    const chats = user.chats.map(({ role, content }) => ({
+      role,
+      content,
+    })) as ChatCompletionRequestMessage[];
+
+    chats.push({ content: message, role: "user" });
+    user.chats.push({ content: message, role: "user" });
+
+    const config = configureOpenAI();
+    const openai = new OpenAIApi(config);
+    const chatResponse = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: chats,
+    });
+    user.chats.push(chatResponse.data.choices[0].message);
+    await user.save();
+    return res.status(200).json({ chats: user.chats });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong." });
   }
-
-  const chats = user.chats.map(({ role, content }) => ({
-    role,
-    content,
-  })) as ChatCompletionRequestMessage[];
-
-  chats.push({ content: message, role: "user" });
-  user.chats.push({ content: message, role: "user" });
-
-  const config = configureOpenAI();
-  const openai = new OpenAIApi(config);
-  const chatResponse = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: chats,
-  });
 };
