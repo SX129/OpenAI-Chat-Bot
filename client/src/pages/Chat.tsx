2e8b402a96712d5c61a7b36ef4e68f7aeedf88d2
Henry Nguyen <nguyenhenry682@gmail.com>
2023-10-31 19:30:03 -0500

    Added styling for chat messages for dif roles

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -3,6 +3,47 @@ import React from "react";
 import { useAuth } from "../context/AuthContext";
 import { red } from "@mui/material/colors";
 
+const chat_messages = [
+  { role: "user", content: "Hello, how are you?" },
+  {
+    role: "assistant",
+    content:
+      "I'm just a computer program, so I don't have feelings, but I'm here to help you! How can I assist you today?",
+  },
+  { role: "user", content: "I need help with a programming problem." },
+  {
+    role: "assistant",
+    content:
+      "Sure! I'd be happy to help. What programming language are you using, and what seems to be the problem?",
+  },
+  {
+    role: "user",
+    content:
+      "I'm working with Python, and I'm getting an error when trying to open a file.",
+  },
+  {
+    role: "assistant",
+    content:
+      "Can you please show me the code where you're trying to open the file, as well as the exact error message you're receiving?",
+  },
+  {
+    role: "user",
+    content:
+      'Here is the code:\n\nwith open("example.txt", "r") as file:\n    content = file.read()\n\nAnd the error message says "FileNotFoundError: [Errno 2] No such file or directory: \'example.txt\'".',
+  },
+  {
+    role: "assistant",
+    content:
+      "It looks like the file 'example.txt' is not in the same directory as your Python script. Make sure the file is located in the same folder, or provide the correct path to the file.",
+  },
+  { role: "user", content: "Ah, I see. Thank you for your help!" },
+  {
+    role: "assistant",
+    content:
+      "You're welcome! If you have any more questions, feel free to ask. Happy coding!",
+  },
+];
+
 const Chat = () => {
   const auth = useAuth();
 
@@ -71,7 +112,12 @@ const Chat = () => {
         </Box>
       </Box>
       <Box
-        sx={{ display: "flex", flex: { md: 0.8, xs: 1, sm: 1 }, flexDirection: "column", px:3 }}
+        sx={{
+          display: "flex",
+          flex: { md: 0.8, xs: 1, sm: 1 },
+          flexDirection: "column",
+          px: 3,
+        }}
       >
         <Typography
           sx={{
@@ -97,7 +143,11 @@ const Chat = () => {
             overflowY: "auto",
             scrollBehavior: "smooth",
           }}
-        ></Box>
+        >
+          {chat_messages.map((chat) => (
+            <div>{chat.content}</div>
+          ))}
+        </Box>
       </Box>
     </Box>
   );
