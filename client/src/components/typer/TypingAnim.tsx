c47fe4504af29156b7562433276e955fd7b7c6e1
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-01 00:48:42 -0500

    Added typing animation component for home page

---
+++ client/src/components/typer/TypingAnim.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+
+const TypingAnim = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed once, initially
+        "A Chat-GPT Clone",
+        1000,
+        "Built with OpenAI - GPT 3.5 Turbo 🤖",
+        2000,
+      ]}
+      speed={50}
+      style={{
+        fontSize: "60px",
+        color: "white",
+        display: "inline-block",
+        textShadow: "1px 1px 20px #000",
+      }}
+      repeat={Infinity}
+    />
+  );
+};
+
+export default TypingAnim;
