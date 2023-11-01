ff5cf0150463e410c14fcaf60b2bb20a581d5de3
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-01 00:51:27 -0500

    Added typingAnim component to Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,25 @@
+import { Box } from "@mui/material";
 import React from "react";
+import TypingAnim from "../components/typer/TypingAnim";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box width={"100%"} height={"100%"} flex={"flex"} mx={"auto"}>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          flexDirection: "column",
+          alignItems: "center",
+          mx: "auto",
+        }}
+      >
+        <Box>
+          <TypingAnim />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Home;
